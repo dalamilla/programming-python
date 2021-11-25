e8a3ab5104dccc31603186b635b6e7f4df050822
Daniel Alamilla <daniel.alamilla.co@gmail.com>
2021-11-24 20:57:14 -0600

    add request header project

--- request-header/main.py
+++ request-header/main.py
@@ -1,8 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Header
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="*"
+)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/api/whoami")
+async def whoami(request: Request, user_agent: str = Header(None), accept_language: str = Header(None)):
+    client_host = request.client.host
+    return {"ipaddress": client_host, "language": accept_language, "software": user_agent}
